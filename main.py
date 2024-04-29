d08f63fc86c7fbb1e81e7c9dd124281c9cbf5796
Vasiliy Zazdravnykh <coder.vz@gmail.com>
2024-04-29 17:47:30 +0300

    add logic

---
+++ main.py
@@ -0,0 +1,73 @@
+
+import urllib.request, json
+import random
+import time
+
+
+BTC_EMISSION = 21000000
+BTC_S = 'BTC'
+
+Currency = {
+    'MIOTA': 3229505319,
+    'SOL': 574915273,
+    'XRP': 100000000000,
+    'TON': 5105886521,
+    'ADA': 45000000000,
+    'AVAX': 715748719,
+    'TRX': 87554275200,
+    'DOT': 1437953431,
+    'BCH': 21000000,
+    'LINK': 1000000000,
+    'UNI': 1000000000,
+    'HBAR': 50000000000,
+    'APT': 1093375145,
+    'FIL': 1960538665,
+    'XLM': 50001806812,
+    'ATOM': 390930671,
+    'VET': 86712634466,
+    'MKR': 1005577,
+    'OP': 4294967296,
+    'GRT': 10797266751,
+    'FTM': 3175000000,
+    'ALGO': 10000000000,
+    'FLOW': 1503293597,
+    'HNT': 223000000,
+    'OM': 888888888,
+    'IOTX': 10000000000,
+    'HOT': 177619433541,
+    'ZEC': 21000000,
+    'NANO': 133248297,
+    'ONT': 1000000000,
+    'CTK': 133687317,
+    'NEAR': 1188295576,
+    'QRL': 105000000,
+    'MATIC': 10000000000,
+    'CRV': 3303030299,
+    'XMR': 18429993
+};
+
+""" 
+Получить цену монеты
+ """
+def getPrice(curr):
+    url = f"https://data-api.cryptocompare.com/asset/v1/data/by/symbol?asset_symbol={curr}&api_key="
+
+    with urllib.request.urlopen(url) as url:
+        data = json.load(url)
+        return data['Data']['PRICE_USD']
+
+
+BTC_price = getPrice(BTC_S)
+
+Profit = {}
+
+for curr, emission in Currency.items(): 
+    time.sleep(2)
+    currency_price = getPrice(curr)
+    
+    underestimate = (BTC_price/(emission/BTC_EMISSION))/currency_price
+    print(curr, currency_price, underestimate)
+    Profit[round(underestimate)] = curr
+    
+sorted_profit = dict(sorted(Profit.items(), reverse=True))
+print(sorted_profit)
